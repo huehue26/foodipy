07d4159c0670fb5b0ebda9a9a1fd9984d942e26b
Somesh Singh <someshsingh298@gmail.com>
2021-10-08 22:33:29 +0530

    added connection in login and signup page

--- js/logic.js
+++ js/logic.js
@@ -17,3 +17,15 @@ document.getElementById('login-button').addEventListener('click',function() {
 document.getElementById('close-login').addEventListener('click',function() {
     document.getElementById('login').style.display='none';
 });
+
+// opening signup when create account is clicked
+document.getElementById('create_account').addEventListener('click',function() {
+    document.getElementById('login').style.display='none';
+    document.getElementById('signup').style.display='flex';
+});
+
+// opening login when already have a account is clicked
+document.getElementById('login_account').addEventListener('click',function() {
+    document.getElementById('signup').style.display='none';
+    document.getElementById('login').style.display='flex';
+});
