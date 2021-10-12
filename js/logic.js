d566143c0f50a03a8af6063120c5902383f63679
Somesh Singh <someshsingh298@gmail.com>
2021-10-13 00:32:34 +0530

    some minor changes

--- js/logic.js
+++ js/logic.js
@@ -1,31 +1,38 @@
 // opening sign up on click
-document.getElementById('signup-button').addEventListener('click',function() {
-    document.getElementById('signup').style.display='flex';
+document.getElementById('signup-button').addEventListener('click', function() {
+    document.getElementById('signup').style.display = 'flex';
 });
 
 // closing sign up after clicking on cross
-document.getElementById('close-signup').addEventListener('click',function() {
-    document.getElementById('signup').style.display='none';
+document.getElementById('close-signup').addEventListener('click', function() {
+    document.getElementById('signup').style.display = 'none';
 });
 
 // opening log in on click
-document.getElementById('login-button').addEventListener('click',function() {
-    document.getElementById('login').style.display='flex';
+document.getElementById('login-button').addEventListener('click', function() {
+    document.getElementById('login').style.display = 'flex';
 });
 
 // closing log in after clicking on cross
-document.getElementById('close-login').addEventListener('click',function() {
-    document.getElementById('login').style.display='none';
+document.getElementById('close-login').addEventListener('click', function() {
+    document.getElementById('login').style.display = 'none';
 });
 
 // opening signup when create account is clicked
-document.getElementById('create_account').addEventListener('click',function() {
-    document.getElementById('login').style.display='none';
-    document.getElementById('signup').style.display='flex';
+document.getElementById('create_account').addEventListener('click', function() {
+    document.getElementById('login').style.display = 'none';
+    document.getElementById('signup').style.display = 'flex';
 });
 
 // opening login when already have a account is clicked
-document.getElementById('login_account').addEventListener('click',function() {
-    document.getElementById('signup').style.display='none';
-    document.getElementById('login').style.display='flex';
+document.getElementById('login_account').addEventListener('click', function() {
+    document.getElementById('signup').style.display = 'none';
+    document.getElementById('login').style.display = 'flex';
+});
+
+
+// checking signup input
+document.getElementById('submit-signup').addEventListener('click', function() {
+    const name = document.getElementById('name-input').innerText;
+    console.log(name);
 });
