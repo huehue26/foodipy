180a58c9a837a1386467ead9f6f574a4a361582c
Somesh Singh <someshsingh298@gmail.com>
2021-11-12 22:51:30 +0530

    added trending

--- js/food.js
+++ js/food.js
@@ -286,7 +286,6 @@ $('#del3').on('click', function () {
 })
 
 
-console.log(foodId);
 // recommended food
 
 for (var i = foodId-1; i >= foodId - 15; i--) {
@@ -361,3 +360,78 @@ for (var i = foodId+1; i <= foodId + 15; i++) {
             });
         });
 }
+
+
+// trending food
+
+fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood`)
+	.then(res => res.json())
+	.then(data => {
+		const recipe = data.meals;
+		for (var i = 0; i < 4; i++) {
+			document.getElementById('trending_food').innerHTML +=
+		`
+		<div class= "img_and_name">
+			<div class="skj_cards">
+				<img src="${recipe[i].strMealThumb}" alt="">
+				<div class="skj_discription">
+					<a href="./food.html">
+						<div class="skj_content">
+							<div class="skj_symbol">
+								<i class="fa fa-shopping-cart skj_icons" style=" margin: 25px 28px 0px 0px; font-size: 35px;"></i>
+								<i class="fa fa-heart skj_icons" style="margin: 25px 0px 0px 28px; font-size:35px;"></i>
+							</div>
+							<div style="display:none;">
+								${recipe[i].idMeal}
+							</div>
+						</div>
+					</a>
+				</div>
+			</div>
+			<div class="cake_name">
+				<h1">${recipe[i].strMeal}</h1>
+			</div>
+		</div>
+	`
+		}
+		$(".skj_content").click(function (e) {
+			current_food_id = e.target.lastElementChild.innerText;
+			sessionStorage.setItem('foodId', current_food_id);
+		});
+	});
+
+    fetch(`www.themealdb.com/api/json/v1/1/filter.php?a=Canadian`)
+	.then(res => res.json())
+	.then(data => {
+		const recipe = data.meals;
+		for (var i = 0; i < 4; i++) {
+			document.getElementById('trending_food').innerHTML +=
+		`
+		<div class= "img_and_name">
+			<div class="skj_cards">
+				<img src="${recipe[i].strMealThumb}" alt="">
+				<div class="skj_discription">
+					<a href="./food.html">
+						<div class="skj_content">
+							<div class="skj_symbol">
+								<i class="fa fa-shopping-cart skj_icons" style=" margin: 25px 28px 0px 0px; font-size: 35px;"></i>
+								<i class="fa fa-heart skj_icons" style="margin: 25px 0px 0px 28px; font-size:35px;"></i>
+							</div>
+							<div style="display:none;">
+								${recipe[i].idMeal}
+							</div>
+						</div>
+					</a>
+				</div>
+			</div>
+			<div class="cake_name">
+				<h1">${recipe[i].strMeal}</h1>
+			</div>
+		</div>
+	`
+		}
+		$(".skj_content").click(function (e) {
+			current_food_id = e.target.lastElementChild.innerText;
+			sessionStorage.setItem('foodId', current_food_id);
+		});
+	});
